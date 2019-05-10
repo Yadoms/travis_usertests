b5724a99dfb8b1d6d4974ef92458644b3d1f4524
jano42 <decoretjm@gmail.com>
2019-05-10 13:05:08 +0200

    Fix upload

--- report.py
+++ report.py
@@ -1,4 +1,7 @@
 import pysftp
 
-with pysftp.Connection('ftp.jano42.fr', username='janofnxr-usertests', password='Usertests2019') as sftp:
-    with sftp.put('/report.html')
+cnopts = pysftp.CnOpts()
+cnopts.hostkeys = None  
+
+with pysftp.Connection('ftp.jano42.fr', username='janofnxr-usertests', password='Usertests2019', cnopts=cnopts) as sftp:
+    sftp.put('/report.html')
